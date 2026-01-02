f2cf636d075444e7fa8ee5546fca5d1ac3c6c066
vatojuan <juanchi190_16@hotmail.com>
2026-01-02 19:08:28 -0300

    02/01-123456789012345678901

--- src/views/ManageDeviceView.vue
+++ src/views/ManageDeviceView.vue
@@ -515,7 +515,7 @@ onMounted(async () => {
 </template>
 
 <style scoped>
-/* FIX DEFINITIVO: Usamos variables globales para que coincida con ChannelsView */
+/* ESTILO CORREGIDO: Usando variables globales del tema en lugar de hardcode */
 
 .page-wrap {
   padding: 1rem;
@@ -610,7 +610,23 @@ select {
   padding: 0.7rem;
   margin-top: 0.3rem;
   outline: none;
+
+  /* Importante: Eliminar estilo nativo para asegurar que tome el fondo */
+  -webkit-appearance: none;
+  appearance: none;
+  /* Restaurar flechita del select si se pierde al quitar apariencia nativa */
+  background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23FFF%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E');
+  background-repeat: no-repeat;
+  background-position: right 0.7em top 50%;
+  background-size: 0.65em auto;
 }
+
+/* Excepción para inputs de texto que no necesitan flecha */
+input[type='text'],
+input[type='number'] {
+  background-image: none;
+}
+
 /* Asegura que las opciones del desplegable también tengan fondo correcto */
 select option {
   background-color: var(--bg-color);
